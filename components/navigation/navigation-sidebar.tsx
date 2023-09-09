f295ff0567d390e64c0eec65632853b42270c14d
Insu Hong <hinsu93@naver.com>
2023-09-09 15:06:40 +0900

    user avatar 추가

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,12 +1,14 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -41,6 +43,17 @@ export const NavigationSidebar = async () => {
             </div>
           ))}
         </ScrollArea>
+        <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+          <ModeToggle />
+          <UserButton
+            afterSignOutUrl="/"
+            appearance={{
+              elements: {
+                avatarBox: "h-[48px] w-[48px]",
+              },
+            }}
+          />
+        </div>
       </div>
     </>
   );
