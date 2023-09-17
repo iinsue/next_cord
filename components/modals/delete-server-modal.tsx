8d25f9be5febd04d026e8b7be7a0a1e661a79ee8
iinsue <hinsu93@naver.com>
2023-09-17 23:02:18 +0900

    서버 삭제 기능 구현

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -16,11 +16,11 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
-export const LeaveServerModal = () => {
+export const DeleteServerModal = () => {
   const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
 
-  const isModalOpen = isOpen && type === "leaveServer";
+  const isModalOpen = isOpen && type === "deleteServer";
   const { server } = data;
 
   const [isLoading, setIsLoading] = useState(false);
@@ -28,7 +28,7 @@ export const LeaveServerModal = () => {
   const onClick = async () => {
     try {
       setIsLoading(true);
-      await axios.patch(`/api/servers/${server?.id}/leave`);
+      await axios.delete(`/api/servers/${server?.id}`);
 
       onClose();
       router.refresh();
@@ -46,14 +46,14 @@ export const LeaveServerModal = () => {
         <DialogContent className="bg-white text-black p-0 overflow-hidden">
           <DialogHeader className="pt-8 px-6">
             <DialogTitle className="text-2xl text-center font-bold">
-              Leave Server
+              Delete Server
             </DialogTitle>
             <DialogDescription className="text-center text-zinc-500">
-              Are you sure you want to leave &nbsp;
+              Are you sure you want to do this? <br />
               <span className="font-semibold text-indigo-500">
                 {server?.name}
               </span>
-              ?
+              &nbsp; will be permanently deleted.
             </DialogDescription>
           </DialogHeader>
           <DialogFooter className="bg-gray-100 px-6 py-4">
