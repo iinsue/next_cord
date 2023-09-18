648fe02f49642b79e46ba36ab75d8840fbe6cd9b
iinsue <hinsu93@naver.com>
2023-09-18 23:17:28 +0900

    사이드 바에 Text Channels 추가

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -3,6 +3,9 @@
 import { ChannelType, MemberRole } from "@prisma/client";
 
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { Plus } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
   label: string;
@@ -12,10 +15,33 @@ interface ServerSectionProps {
   server?: ServerWithMembersWithProfiles;
 }
 
-export const ServerSection = () => {
+export const ServerSection = ({
+  label,
+  role,
+  sectionType,
+  channelType,
+  server,
+}: ServerSectionProps) => {
+  const { onOpen } = useModal();
+
   return (
     <>
-      <div>Server Section!</div>
+      <div className="flex items-center justify-between py-2">
+        <p className="text-xs uppercase font-semibold text-zinc-500 dark:text-zinc-400">
+          {label}
+        </p>
+        {role !== MemberRole.GUEST && sectionType === "channels" && (
+          <ActionTooltip label="Create Channel" side="top">
+            <button
+              onClick={() => onOpen("createChannel")}
+              className="text-zinc-500 hover:text-zinc-600 
+            dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+            >
+              <Plus className="h-4 w-4" />
+            </button>
+          </ActionTooltip>
+        )}
+      </div>
     </>
   );
 };
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -131,7 +131,12 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
           <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
           {!!textChannels?.length && (
             <div className="mb-2">
-              <ServerSection />
+              <ServerSection
+                sectionType="channels"
+                channelType={ChannelType.TEXT}
+                role={role}
+                label="Text Channels"
+              />
             </div>
           )}
         </ScrollArea>
